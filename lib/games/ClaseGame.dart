58025788855d291d3b1b58532e5687fb04f72db6
carlos <carlosguemesdam@gmail.com>
2024-01-11 17:42:40 +0100

    Commit V5: Cargar imágenes y backgroundColor en ClaseGame

--- lib/games/ClaseGame.dart
+++ lib/games/ClaseGame.dart
@@ -1,10 +1,26 @@
+import 'dart:ui';
+
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 
-class ClaseGame extends FlameGame with HasKeyboardHandlerComponents{
+class ClaseGame extends FlameGame with HasKeyboardHandlerComponents {
   ClaseGame();
 
+  @override
+  Color backgroundColor() {
+    return const Color.fromARGB(255, 173, 223, 255);
+  }
 
-
-
+  @override
+  Future<void> onLoad() async {
+    await images.loadAll([
+      'ember.png',
+      'heart_half.png',
+      'heart.png',
+      'star.png',
+      'water_enemy.png',
+      'mapa_prueba.png',
+      'mapa_bloques.png'
+    ]);
+  }
 }
