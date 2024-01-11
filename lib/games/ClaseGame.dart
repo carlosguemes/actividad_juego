22425aa829326b7ffb8f7e4e589a29121df2ea7b
carlos <carlosguemesdam@gmail.com>
2024-01-11 17:47:02 +0100

    Commit V6: Cargar mapa

--- lib/games/ClaseGame.dart
+++ lib/games/ClaseGame.dart
@@ -1,10 +1,25 @@
 import 'dart:ui';
 
+import 'package:flame/camera.dart';
+import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:flame_tiled/flame_tiled.dart';
 
 class ClaseGame extends FlameGame with HasKeyboardHandlerComponents {
   ClaseGame();
+
+  final world = World();
+  late final CameraComponent cameraComponent;
+  late TiledComponent mapComponent;
+
+  double gameWidth = 1920;
+  double gameHeigth = 1080;
+
+  double wScale = 1.0;
+  double hScale = 1.0;
+
+  late double tamanyo;
 
   @override
   Color backgroundColor() {
@@ -22,5 +37,19 @@ class ClaseGame extends FlameGame with HasKeyboardHandlerComponents {
       'mapa_prueba.png',
       'mapa_bloques.png'
     ]);
+
+    wScale = size.x/gameWidth;
+    hScale = size.y/gameHeigth;
+
+    cameraComponent = CameraComponent(world: world);
+    // Everything in this tutorial assumes that the position
+    // of the `CameraComponent`s viewfinder (where the camera is looking)
+    // is in the top left corner, that's why we set the anchor here.
+    cameraComponent.viewfinder.anchor = Anchor.topLeft;
+    addAll([cameraComponent, world]);
+
+    mapComponent = await TiledComponent.load('mapa_prueba.tmx', Vector2(32*wScale, 32*hScale));
+    world.add(mapComponent);
   }
+
 }
